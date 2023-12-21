22ca228917a9eff432f371d3c966bea1b05d7820
Ilona <rvl.arg@gmail.com>
2023-12-21 10:40:06 +0100

    modif interface

--- interface.py
+++ interface.py
@@ -1,1 +1,18 @@
 import pyxel
+
+class interface:
+
+    def __init__(self):
+        pyxel.init(128,128, title="Jeu de la vie")
+        pyxel.run(self.update, self.draw)
+    
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+class Grille:
+    pass
+
+interface()
