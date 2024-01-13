00e19b0b68ca6ddccf528968b2a2b1dda94ff32c
Lunosome <90963599+Lunosome@users.noreply.github.com>
2024-01-13 17:12:05 +0100

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,6 +1,7 @@
 import pyxel
 from grille import Grille, Cellule
 
+CASE = 6
 class interface:
 
     def __init__(self):
@@ -8,10 +9,16 @@ class interface:
         pyxel.run(self.update, self.draw)
     
     def update(self):
-        pass
+        Grille.tour()
+    
 
     def draw(self):
-        pass
+        def draw(self):
+        pyxel.cls(0)
+        for x in range(Grille.c.x):
+            for y in range(Grille.c.y):
+                if Grille.c[x][y].vivante:
+                    pyxel.rect(x * CASE, y * CASE, CASE, CASE, 7)
 
 
 
