3d3578ecec0ac6ffead112785ba12574de1c89a7
Lunosome <90963599+Lunosome@users.noreply.github.com>
2024-01-21 11:29:20 +0100

    Update interface.py

--- interface.py
+++ interface.py
@@ -2,24 +2,22 @@ import pyxel
 from grille import Grille, Cellule
 
 CASE = 6
-class interface:
+
+class Interface:
 
     def __init__(self):
-        pyxel.init(128,128, title="Jeu de la vie")
+        self.grille = Grille(128 // CASE, 128 // CASE)  # Crée une instance de Grille
+        pyxel.init(128, 128, title="Jeu de la vie")
         pyxel.run(self.update, self.draw)
-    
+
     def update(self):
-        Grille.tour()
-    
+        self.grille.tour()  # Utilise la méthode tour de l'instance de Grille
 
     def draw(self):
-        def draw(self):
         pyxel.cls(0)
-        for x in range(Grille.c.x):
-            for y in range(Grille.c.y):
-                if Grille.c[x][y].vivante:
+        for x in range(self.grille.largeur):
+            for y in range(self.grille.hauteur):
+                if self.grille.get_cellule(x, y).viv:
                     pyxel.rect(x * CASE, y * CASE, CASE, CASE, 7)
 
-
-
-interface()
+Interface()
